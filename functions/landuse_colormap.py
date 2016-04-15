4567225416f729c41240e11a14be7acc735b05e7
Brian Blaylock <blaylockbk@users.noreply.github.com>
2016-04-15 13:29:40 -0600

    Update landuse_colormap.py

--- functions/landuse_colormap.py
+++ functions/landuse_colormap.py
@@ -1,10 +1,10 @@
-## Custom Landuse ColorMaps by Brian
+## Custom Terrain ColorMaps by Brian
 
 import matplotlib.pyplot as plt
 from matplotlib.colors import ListedColormap
 import numpy as np
 
-# Four Different Land Use Categories you can use in WRF:
+# Four Different Land Use Categories:
 #   LU_MODIS20
 #   LU_MODIS21     includes lake category
 #   LU_USGS
@@ -31,12 +31,13 @@ def LU_MODIS21(): #
     [.7,.9,.3],      #! 14 Cropland/Natual Vegation Mosaic
     [1,1,1],        #! 15 Snow and Ice
     [.914,.914,.7], #  16 Barren or Sparsely Vegetated
-    [0,0,.88],        #  17 Water
+    [.5,.7,1],        #  17 Water (like oceans)
     [.86,.08,.23],        #  18 Wooded Tundra
     [.97,.5,.31],        #! 19 Mixed Tundra
     [.91,.59,.48],     #! 20 Barren Tundra
-    [.5,.7,1]])    #! 21 Lake
-
+    [0,0,.88]])      #! 21 Lake
+    
+   
     cm = ListedColormap(C)
     
     labels = ['Evergreen Needleleaf Forest',
@@ -65,26 +66,26 @@ def LU_MODIS21(): #
 
 def LU_MODIS20(): #
     C = np.array([
-    [0,.4,0],      #  1 Evergreen Needleleaf Forest
-    [0,.4,.2],      #! 2 Evergreen Broadleaf Forest    
-    [.2,.8,.2],     #  3 Deciduous Needleleaf Forest
-    [.2,.8,.4],     #  4 Deciduous Broadleaf Forest
-    [.2,.6,.2],     #  5 Mixed Forests
-    [.3,.7,0],      #  6 Closed Shrublands
-    [.82,.41,.12],     #  7 Open Shurblands
-    [.74,.71,.41],       #  8 Woody Savannas
-    [1,.84,.0],     #  9 Savannas
-    [0,1,0],        #  10 Grasslands
-    [0,1,1],        #! 11 Permanant Wetlands
-    [1,1,0],      #  12 Croplands
-    [1,0,0],     #  13 Urban and Built-up
-    [.7,.9,.3],      #! 14 Cropland/Natual Vegation Mosaic
-    [1,1,1],        #! 15 Snow and Ice
-    [.914,.914,.7], #  16 Barren or Sparsely Vegetated
+    [0,.4,0],         #  1 Evergreen Needleleaf Forest
+    [0,.4,.2],        #! 2 Evergreen Broadleaf Forest    
+    [.2,.8,.2],       #  3 Deciduous Needleleaf Forest
+    [.2,.8,.4],       #  4 Deciduous Broadleaf Forest
+    [.2,.6,.2],       #  5 Mixed Forests
+    [.3,.7,0],        #  6 Closed Shrublands
+    [.82,.41,.12],    #  7 Open Shurblands
+    [.74,.71,.41],    #  8 Woody Savannas
+    [1,.84,.0],       #  9 Savannas
+    [0,1,0],          #  10 Grasslands
+    [0,1,1],          #! 11 Permanant Wetlands
+    [1,1,0],          #  12 Croplands
+    [1,0,0],          #  13 Urban and Built-up
+    [.7,.9,.3],       #! 14 Cropland/Natual Vegation Mosaic
+    [1,1,1],          #! 15 Snow and Ice
+    [.914,.914,.7],   #  16 Barren or Sparsely Vegetated
     [0,0,.88],        #  17 Water
-    [.86,.08,.23],        #  18 Wooded Tundra
-    [.97,.5,.31],        #! 19 Mixed Tundra
-    [.91,.59,.48]])     #! 20 Barren Tundra
+    [.86,.08,.23],    #  18 Wooded Tundra
+    [.97,.5,.31],     #! 19 Mixed Tundra
+    [.91,.59,.48]])   #! 20 Barren Tundra
 
     cm = ListedColormap(C)
     
@@ -110,9 +111,66 @@ def LU_MODIS20(): #
               'Barren Tundra']
     
     return cm, labels
+ 
+def LU_USGS24():
     
+    C = np.array([
+    [1,0,0],          #  1 Urban and Built-up Land
+    [1,1,0],          #! 2 Dryland Cropland and Pasture
+    [1,1,.2],         #  3 Irrigated Cropland and Pasture
+    [1,1,.3],         #  4 Mixed Dryland/Irrigated Cropland and Pasture
+    [.7,.9,.3],       #  5 Cropland/Grassland Mosaic
+    [.7,.9,.3],       #  6 Cropland/Woodland Mosaic
+    [0,1,0],          #  7 Grassland
+    [.3,.7,0],        #  8 Shrubland
+    [.82,.41,.12],    #  9 Mixed Shrubland/Grassland
+    [1,.84,.0],       #  10 Savanna
+    [.2,.8,.4],       #  11 Deciduous Broadleaf Forest
+    [.2,.8,.2],       #  12 Deciduous Needleleaf Forest
+    [0,.4,.2],        #  13 Evergreen Broadleaf Forest
+    [0,.4,0],         #! 14 Evergreen Needleleaf Forest 
+    [.2,.6,.2],       #  15 Mixed Forests
+    [0,0,.88],        #  16 Water Bodies
+    [0,1,1],          #! 17 Herbaceous Wetlands
+    [.2,1,1],         #  18 Wooden Wetlands
+    [.914,.914,.7],   #  19 Barren or Sparsely Vegetated
+    [.86,.08,.23],    #  20 Herbaceous Tundraa
+    [.86,.08,.23],    #  21 Wooded Tundra
+    [.97,.5,.31],     #! 22 Mixed Tundra
+    [.91,.59,.48],   #! 23 Barren Tundra
+    [1,1,1]          #! 24 Snow and Ice
+    ])
     
-def LU_USGS():
+    cm = ListedColormap(C)
+    
+    labels = ['Urban and Built-up Land',
+              'Dryland Cropland and Pasture',
+              'Irrigated Cropland and Pasture',
+              'Mixed Dryland/Irrigated Cropland and Pasture',
+              'Cropland/Grassland Mosaic',
+              'Cropland/Woodland Mosaic',
+              'Grassland',
+              'Shrubland',
+              'Mixed Shrubland/Grassland',
+              'Savanna',
+              'Deciduous Broadleaf Forest',
+              'Deciduous Needleleaf Forest',
+              'Evergreen Broadleaf',
+              'Evergreen Needleleaf',
+              'Mixed Forest',
+              'Water Bodies',
+              'Herbaceous Wetland',
+              'Wooden Wetland',
+              'Barren or Sparsely Vegetated',
+              'Herbaceous Tundra',
+              'Wooded Tundra',
+              'Mixed Tundra',
+              'Bare Ground Tundra',
+              'Snow or Ice']
+    
+    return cm, labels   
+    
+def LU_USGS_chris():
     # Provided by Chris Foster
     C = np.array([
     [0.0, 0.0, 1.0],
@@ -145,7 +203,7 @@ def LU_USGS():
     
     return cm, labels
     
-def LU_NLCD():
+def LU_NLCD_chris():
     # Provided by Chris Foster
     C = np.array([
     [0.0, 0.0, 1.0],
@@ -184,9 +242,9 @@ def LU_NLCD():
 
 if __name__ == "__main__":
     
-    # Example Usage. Take note of the special parameters in pcolormesh and colorbar. There is no category '0' so start with 1
     
-    # Grab colormap and labels
+    
+    # Grab colormap and labels    
     cm,labels = LU_MODIS21()
     
     #create some Land Use category field
@@ -194,11 +252,11 @@ if __name__ == "__main__":
     
     plt.figure(1)
     plt.title('Land Use Categories')
-    plt.pcolormesh(LU_INDEX,cmap = cm,vmin=1,vmax=len(labels)+1)
-    cbar = plt.colorbar()
-    cbar.set_ticks(np.arange(0.5,len(labels)+1)) # puts tick in middle of colored box rather than top
+    plt.pcolormesh(LU_INDEX,cmap = cm,vmin=1,vmax=len(labels)+1) # for example
+    cbar = plt.colorbar(shrink=2)
+    cbar.set_ticks(np.arange(0.5,len(labels)+1))
     cbar.ax.set_yticklabels(labels)
-    #cbar.ax.set_yticklabels(np.arange(1,len(labels)+1))
+    #cbar.ax.set_yticklabels(labels) #If using a horizontal colorbar orientation
+    cbar.ax.invert_yaxis()
     
-
-    
+    cbar.ax.tick_params(labelsize=15) 
